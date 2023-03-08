d02bcd7724eef44d7508654fe7549a4f122c9644
mariami <mariam.kavtaradze978@gmail.com>
2023-03-08 22:49:17 +0400

    add file

---
+++ script.js
@@ -0,0 +1,47 @@
+// function ele_ment(num1, num2, ...numbers)
+// {
+//     let sum=num1+num2;
+//     let mult = 1; 
+//     for(let num of numbers){
+//         mult*=num;
+
+//     }
+//     return{
+//         "jami":sum,
+//         "namravli":mult,
+
+//     }
+// }
+// console.log(ele_ment(4,7,5,9,2,3,8))
+
+///////////////////////////////////////////
+
+// const user = {
+//     banks:[
+//         {},
+//         {},
+//         {address:{city:"Tbilisi"}}
+//     ]
+// };
+
+// const{banks,fullName} = user;
+// console.log(user.banks[2].address.city,fullName);
+
+/////////////////////////////////////////
+
+// const array1 = {
+//     name:"Mariam",
+//     username:"Kavtaradze",
+//     city:"Tbilisi",
+//     country:"Georgia",
+//     planet:"Earth"
+
+// }
+
+// const user = {
+//     ...array1
+// };
+
+// console.log(array1, user)
+// console.log(array1.country,user.planet);
+
